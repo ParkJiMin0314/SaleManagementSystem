0abc6cd2b5e1743f3a0b4ad0c592adc75a82313e
parkjimin <qkrwlals28@gmailc.om>
2023-03-19 20:54:16 +0900

    added Sale.java

---
+++ src/Sale.java
@@ -0,0 +1,11 @@
+public class Sale{
+	   public static void main(String[] args) {
+	   
+	      
+	      System.out.println("1. select menu");
+	      System.out.println("2. delete menu");
+	      System.out.println("3. select side");
+	      System.out.println("4. select juice");
+	      System.out.println("5. exit");
+	   }
+	}
