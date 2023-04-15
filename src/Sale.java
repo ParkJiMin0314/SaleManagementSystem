39bbfe62147e9ae253561b37f3d6f2cc26ee8f72
parkjimin <qkrwlals28@gmailc.om>
2023-04-15 20:02:01 +0900

    added.Sale.java

--- src/Sale.java
+++ src/Sale.java
@@ -1,54 +1,39 @@
 import java.util.Scanner;
 
-public class Sale
-{
-	public static void main(String[] args)
-	{
-        Scanner input1 = new Scanner(System.in);
-        System.out.println("please order: food, drink");
-        String a = input1.next();
-        
-        int b;
-        
-        switch (a)
-        {
-        case "food":
-        b = 1;
-        break;
-        
-        case "drink":
-        b = 2;
-        break;
-        
-        default:
-        b = 3;
-        }
-        
-        if (b == 1)
-        {
-           menu();
-        }
-        
-        else if (b == 2)
-        {
-           juice();
+public class Sale {
 
-        }
+	public static void main(String[] args) {
+
+		int num = 0;
+	      Scanner input = new Scanner(System.in);
+	      SaleManagement sale = new SaleManagement(input);
+	      while(num != 5) {
+	         System.out.println("hamburger sale system");
+	         System.out.println("1.add burger");
+	         System.out.println("2.delete order history");
+	         System.out.println("3.edit order history");
+	         System.out.println("4.view order history");
+	         System.out.println("5.exit");
+	         System.out.println("select one number 1-5");
+	         num = input.nextInt();
+	         
+	         switch(num) {
+	         case 1:
+	            sale.addMenu();
+	            break;
+	         case 2:
+	            sale.deleteMenu();
+	            break;
+	         case 3:
+	            sale.editMenu();
+	            break;
+	         case 4:
+	            sale.viewMenu();
+	            break;
+	         }
+	      }
+
+		
 	}
-	   
-	public static void menu()
-	{
-        Scanner input2 = new Scanner(System.in);
-        System.out.println("kimbob, ramen, fried rice");
-        String c= input2.next();
-        System.out.println("you ordered "+c);
-	}
-	
-	public static void juice()
-	{
-		Scanner input3 = new Scanner(System.in);
-        System.out.println("coke, sprite, fanta");
-        String d = input3.next();
-        System.out.println("you ordered "+d);
-	}
+
 }
