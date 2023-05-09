3b24dbb8c73a03deb68d73d45a35eed50d700138
parkjimin <qkrwlals28@gmailc.om>
2023-05-09 16:09:10 +0900

    test

---
+++ src/sale/PlusMenu.java
@@ -0,0 +1,25 @@
+package sale;
+
+import java.util.ArrayList;
+
+public class PlusMenu extends Menu{
+   double burgerCost;
+   double juiceCost;
+   Cost c = new Cost();
+   Student std = new Student();
+   
+   public void setHamburger(String hamburger) {
+      c.setBurgerCost(hamburger);
+      this.hamburger = hamburger;
+   }
+   
+   public void setJuice(String juice) {
+      c.setJuiceCost(juice);
+      this.juice = juice;
+   }
+   public void printInfo() {
+      System.out.println("name: " + name + " phonenumber: " + phonenumber + " hamburger: " + hamburger + " juice: " + juice);
+      System.out.println("burgerCost is : $" + c.getBurgerCost() + " juiceCost  is: $" + c.getJuiceCost());
+
+   }
+}
