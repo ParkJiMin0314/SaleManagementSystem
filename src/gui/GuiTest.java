18018d6ba1c554c56c6968ba9ba4d1d97cb954aa
parkjimin <qkrwlals28@gmailc.om>
2023-06-04 17:22:57 +0900

    add. java. Sale

---
+++ src/gui/GuiTest.java
@@ -0,0 +1,16 @@
+package gui;
+
+public class GuiTest {
+
+   public static void main(String[] args) {
+	//   UniversityGui u = new UniversityGui();
+	//   HighschoolGui h = new HighschoolGui();
+	//   MiddleSchoolGui m = new MiddleSchoolGui();
+	//   ElementarySchoolGui e = new ElementarySchoolGui();
+	   
+	   ManagementGui M = new ManagementGui();
+	//   ViewGui v = new ViewGui();
+	//   EditGui e = new EditGui();
+   }
+
+}
